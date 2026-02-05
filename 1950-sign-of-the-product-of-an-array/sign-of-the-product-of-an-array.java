b3ce55b0f5815b75d83d5c067c62d19e8e5b2e71
jiaqian2004 <113960538+jiaqian2004@users.noreply.github.com>
2026-02-05 13:44:32 +0800

    Time: 1 ms (23.87%) | Memory: 45.1 MB (93.44%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -8,10 +8,10 @@ class Solution {
         int negative =1;
         for(int i =0; i<nums.length;i++){
             if(nums[i] ==0){
-                return 0;
+                return signFunc(0);
             }
             else if(nums[i]<0){
-                negative *= -1;
+                negative = -negative;
             }
         }
         return signFunc(negative);
