45cd7332f07ec3b7b61464058bfd37875af70af9
jiaqian2004 <113960538+jiaqian2004@users.noreply.github.com>
2026-02-05 14:43:08 +0800

    Time: 2 ms (90.95%) | Memory: 43.4 MB (89.33%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,25 +1,24 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Deque<Integer> stack = new ArrayDeque<>();
+        List<Integer> records = new ArrayList<>();
         int sumOfAll = 0;
 
-for (String op : operations) {
-    if (op.equals("+")) {
-        int a = stack.pop();
-        int b = stack.peek();
-        stack.push(a);
-        stack.push(a + b);
-    } else if (op.equals("D")) {
-        stack.push(stack.peek() * 2);
-    } else if (op.equals("C")) {
-        stack.pop();
-    } else {
-        stack.push(Integer.parseInt(op));
-    }
-}
+        for(String operation: operations){    
+            if(operation.equals("+")){
+                int sum = records.get(records.size()-2) + records.get(records.size()-1);
+                records.add(sum);
+            }else if(operation.equals("D")){
+                int product = records.get(records.size()-1) *2;
+                records.add(product);
+            }else if(operation.equals("C")){
+                records.remove(records.size()-1);
+            }else{
+                records.add(Integer.parseInt(operation));
+            }
+        }
                 
-        for (int stacks : stack) {
-            sumOfAll += stacks;
+        for (int record : records) {
+            sumOfAll += record;
         }
         return sumOfAll;
 
